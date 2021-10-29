290be98877eea84e13af7b61a9dec44c2594c1f6
farhanz17 <mufhan623@gmail.com>
2021-10-29 11:33:16 +0700

    ini rumus

---
+++ rumus.py
@@ -0,0 +1,8 @@
+##Jari-jarinya adalah 5
+import math
+r = float(input("jari-jarinya adalah : "))
+luas = math.pi*r*r
+keliling = 2*math.pi*r
+
+print("Luas Lingkaran =",format(luas,'.2f'))
+print("keliling lingkaran = ", format(keliling,'.2f'))
